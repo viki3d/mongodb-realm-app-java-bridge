e8f1a18bf3584c6f25db7674accba706918e3ad9
Victor Kirov <victor.kirov.eu@gmail.com>
2026-05-15 16:24:36 +0300

    MongoDbQueryExecutor

--- src/main/java/com/viki3d/mongodb/vibookstore/javabridge/MongoDbQueryExecutable.java
+++ src/main/java/com/viki3d/mongodb/vibookstore/javabridge/MongoDbQueryExecutable.java
@@ -4,15 +4,15 @@ package com.viki3d.mongodb.vibookstore.javabridge;
  * Execute a query against MongoDB.
  */
 public interface MongoDbQueryExecutable {
-	
-	  /**
-	   * Execute MongoDB query, fulfilling RealmApp function.
-	   * Prints the MongoDB query result in JSON format in console. 
-	   * Errors are logged in JSON 'error' tag and system.err.
-	   * 
-	   * @param args The java application input arguments
-	   * 
-	   */
-	  public void execute(String[] args);
+
+  /**
+   * Execute MongoDB query, fulfilling RealmApp function.
+   * Prints the MongoDB query result in JSON format in console. 
+   * Errors are logged in JSON 'error' tag and system.err.
+   *
+   * @param args The java application input arguments
+   * 
+   */
+  public void execute(String[] args);
 
 }
---
+++ src/main/java/com/viki3d/mongodb/vibookstore/javabridge/MongoDbQueryExecutor.java
@@ -0,0 +1,16 @@
+package com.viki3d.mongodb.vibookstore.javabridge;
+
+/**
+ * Descr.
+ *
+ * @author viki3d
+ *
+ */
+public class MongoDbQueryExecutor implements MongoDbQueryExecutable {
+
+  //@Override
+  public void execute(String[] args) {
+
+  }
+
+}
